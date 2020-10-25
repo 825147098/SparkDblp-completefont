165760f0d18c227c37440ff9268dd703207e8314
Kirin <825147098@qq.com>
2020-10-25 22:51:59 +0800

    接入监控

--- src/main.js
+++ src/main.js
@@ -5,6 +5,15 @@ import store from './store'
 import './plugins/element.js'
 // 引入echarts
 import echarts from 'echarts'
+import wpkReporter from 'wpk-reporter' // 导入基础sdk
+
+const __wpk = new wpkReporter({
+  bid: '8sfk7rk3-v22lqxg1', // 新建应用时确定
+  plugins: []
+})
+
+__wpk.installAll() // 初始化sdk 必须调用
+
 // 挂载到vue原型
 Vue.prototype.$echarts = echarts;
 
