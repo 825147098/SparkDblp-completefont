48aa6e0465b67940787ee7527daa1197b2c1a496
linmouhan <825147098@qq.com>
2020-10-25 21:21:00 +0800

    修改

--- src/views/charts/Charts.vue
+++ src/views/charts/Charts.vue
@@ -24,10 +24,10 @@
                     {
                         name: '文章',
                         type: 'line',
-                        symbol: 'trigger',
-                        symbolSize:8,
+                         symbol: 'line',
+                        symbolSize:0,
                         itemStyle: {
-                            borderWidth: 3,
+                            // borderWidth: 3,
                             borderColor: 'black',
                             color: 'gray'
                         },
@@ -37,89 +37,90 @@
                         },
                         data: [[9286, 201], [13009, 3990], [777, 91], [3653, 2610], [0, 24], [216, 678], [69, 194], [619, 1710], [6420, 5390], [24, 109], [15253, 8360], [750, 586], [1, 68], [4971, 861], [20395, 6180], [23301, 4870], [27833, 7470], [34466, 8820], [29683, 6690], [21997, 2860]]
                     },
-                    {
-                        name: '作者',
-                        type: 'line',
-                        symbol: 'circle',
-                        symbolSize:8,
-                        itemStyle: {
-                            borderWidth: 3,
-                            borderColor: 'black',
-                            color: 'gray'
-                        },
-                        lineStyle: {
-                            color: 'gray',
-                            width: 2,
-                        },
-                        data: [[11695, 2540], [26099, 536], [2189, 168], [9173, 4990], [0, 51], [504, 677], [181, 180], [1429, 2080], [11701, 7050], [105, 105], [33066, 5010], [1346, 694], [4, 66], [2472, 3470], [40972, 23470], [36445, 12550], [59676, 15120], [57620, 17900], [57486, 17130], [33979, 8440]]
-                    },
-                    {
-                        name: '会议',
-                        type: 'line',
-                        symbol: 'triangle',
-                        symbolSize:8,
-                        itemStyle: {
-                            borderWidth: 3,
-                            borderColor: 'black',
-                            color: 'gray'
-                        },
-                        lineStyle: {
-                            color: 'gray',
-                            width: 2,
-                        },
-                        data: [[1513, 6260], [2210, 149], [429, 114], [1346, 4250], [0, 45], [167, 672], [55, 176], [394, 2080], [1630, 6990], [21, 202], [2394, 5210], [344, 738], [1, 58], [582, 3320], [2927, 17110], [2996, 18330], [3459, 13930], [3303, 14010], [3349, 10430], [2394, 8940]]
-                    },
+                    // {
+                    //     name: '作者',
+                    //     type: 'line',
+                    //     symbol: 'circle',
+                    //     symbolSize:8,
+                    //     itemStyle: {
+                    //         borderWidth: 3,
+                    //         borderColor: 'black',
+                    //         color: 'gray'
+                    //     },
+                    //     lineStyle: {
+                    //         color: 'gray',
+                    //         width: 2,
+                    //     },
+                    //     data: [[11695, 2540], [26099, 536], [2189, 168], [9173, 4990], [0, 51], [504, 677], [181, 180], [1429, 2080], [11701, 7050], [105, 105], [33066, 5010], [1346, 694], [4, 66], [2472, 3470], [40972, 23470], [36445, 12550], [59676, 15120], [57620, 17900], [57486, 17130], [33979, 8440]]
+                    // },
+                    // {
+                    //     name: '会议',
+                    //     type: 'line',
+                    //     symbol: 'triangle',
+                    //     symbolSize:8,
+                    //     itemStyle: {
+                    //         borderWidth: 3,
+                    //         borderColor: 'black',
+                    //         color: 'gray'
+                    //     },
+                    //     lineStyle: {
+                    //         color: 'gray',
+                    //         width: 2,
+                    //     },
+                    //     data: [[1513, 6260], [2210, 149], [429, 114], [1346, 4250], [0, 45], [167, 672], [55, 176], [394, 2080], [1630, 6990], [21, 202], [2394, 5210], [344, 738], [1, 58], [582, 3320], [2927, 17110], [2996, 18330], [3459, 13930], [3303, 14010], [3349, 10430], [2394, 8940]]
+                    // },
                 ],
 
                 seriesDataC: [
                     {
                         name: '文章_C',
                         type: 'line',
-                        symbol: 'trigger',
-                        symbolSize:8,
+                        symbol:'image://' + require('@/assets/dotted.jpg'),
+                        symbolSize:0,
                         itemStyle: {
-                            borderWidth: 3,
+                            // borderWidth: 3,
                             borderColor: 'black',
                             color: 'gray'
                         },
                         lineStyle: {
                             color: 'gray',
+                            type:'dashed',
                             width: 2,
                         },
                         data: [[9286, 161], [13009, 298], [777, 191.5], [3653, 311.5], [0, 35], [216, 177.5], [69, 93.5], [619, 139], [6420, 272], [24, 101.5], [15253, 251.5], [750, 92], [1, 43], [4971, 310], [20395, 563.5], [23301, 1190], [27833, 2030], [34466, 2755], [29683, 1780], [21997, 411.5]]
                     },
-                    {
-                        name: '作者_C',
-                        type: 'line',
-                        symbol: 'circle',
-                        symbolSize:8,
-                        itemStyle: {
-                            borderWidth: 3,
-                            borderColor: 'black',
-                            color: 'gray'
-                        },
-                        lineStyle: {
-                            color: 'gray',
-                            width: 2,
-                        },
-                        data: [[11695, 158.5], [26099, 490.5], [2189, 115.5], [9173, 152], [0, 24], [504, 106.5], [181, 51], [1429, 74], [11701, 343], [105, 59.5], [33066, 304], [1346, 15], [4, 37.5], [2472, 35], [40972, 250.5], [36445, 298], [59676, 473], [57620, 414], [57486, 239.5], [33979, 171]]
-                    },
-                    {
-                        name: '会议_C',
-                        type: 'line',
-                        symbol: 'triangle',
-                        symbolSize:8,
-                        itemStyle: {
-                            borderWidth: 3,
-                            borderColor: 'black',
-                            color: 'gray'
-                        },
-                        lineStyle: {
-                            color: 'gray',
-                            width: 2,
-                        },
-                        data: [[1513, 60.5], [2210, 108.5], [429, 110], [1346, 54.5], [0, 27], [167, 104.5], [55, 89], [394, 52], [1630, 196.5], [21, 50], [2394, 34.5], [344, 13], [1, 59], [582, 31], [2927, 79.5], [2996, 81], [3459, 100], [3303, 124], [3349, 50], [2394, 87.5]]
-                    },
+                    // {
+                    //     name: '作者_C',
+                    //     type: 'line',
+                    //     symbol: 'circle',
+                    //     symbolSize:8,
+                    //     itemStyle: {
+                    //         borderWidth: 3,
+                    //         borderColor: 'black',
+                    //         color: 'gray'
+                    //     },
+                    //     lineStyle: {
+                    //         color: 'gray',
+                    //         width: 2,
+                    //     },
+                    //     data: [[11695, 158.5], [26099, 490.5], [2189, 115.5], [9173, 152], [0, 24], [504, 106.5], [181, 51], [1429, 74], [11701, 343], [105, 59.5], [33066, 304], [1346, 15], [4, 37.5], [2472, 35], [40972, 250.5], [36445, 298], [59676, 473], [57620, 414], [57486, 239.5], [33979, 171]]
+                    // },
+                    // {
+                    //     name: '会议_C',
+                    //     type: 'line',
+                    //     symbol: 'triangle',
+                    //     symbolSize:8,
+                    //     itemStyle: {
+                    //         borderWidth: 3,
+                    //         borderColor: 'black',
+                    //         color: 'gray'
+                    //     },
+                    //     lineStyle: {
+                    //         color: 'gray',
+                    //         width: 2,
+                    //     },
+                    //     data: [[1513, 60.5], [2210, 108.5], [429, 110], [1346, 54.5], [0, 27], [167, 104.5], [55, 89], [394, 52], [1630, 196.5], [21, 50], [2394, 34.5], [344, 13], [1, 59], [582, 31], [2927, 79.5], [2996, 81], [3459, 100], [3303, 124], [3349, 50], [2394, 87.5]]
+                    // },
                 ]
 
 
@@ -138,8 +139,31 @@
                         return a[0] - b[0]
                     })
 
+                    let a = 49.9;
+                    let b = 0.451;
                     item.data = item.data.map(function (item2) {
-                        return [Math.log(item2[0]), item2[1]]
+                        let ln = Math.log(item2[0]);
+                        let lny = b * ln;
+                        // console.log(lny)
+                        return [ln, a * Math.exp(lny)];
+                    })
+
+                    return {
+                        name: item.name,
+                        type: item.type,
+                        data: item.data
+                    }
+                })
+
+                this.seriesDataC.map(function (item) {
+                    item.data = item.data.sort(function (a, b) {
+                        return a[0] - b[0]
+                    })
+
+                    let a = 3.229E-7;
+                    let b = 2.176;
+                    item.data = item.data.map(function (item2) {
+                        return [Math.log(item2[0]), a * Math.exp(b * Math.log(item2[0]))];
                     })
 
                     return {
@@ -149,9 +173,10 @@
                     }
                 })
                 // console.log(this.seriesData)
+                this.seriesData.push(this.seriesDataC[0]);
                 var option = {
                     title: {
-                        text: this.title + '(无缓存)'
+                        text: this.title + '对比'
                     },
                     tooltip: {
                         trigger: 'axis'
